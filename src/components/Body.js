34d654fc0191aecd377fa3e2fca2cdfdbe94b194
abhi-ragh <abhiragh0@gmail.com>
2024-07-15 00:36:55 +0530

    static webpage complete(kinda)

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,11 @@
 export default function Body(){
     return(
-        <h1>Body</h1>
+        <div className="body--main">
+            <div className="body--hello">
+                <p className="body--greeting">Hello There, Im</p>
+                <p className="body--name">Abhiragh A R</p>
+                <p className="body--def"> {">"} Code Seeker</p>
+            </div>
+        </div>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,14 @@
 export default function Footer(){
     return( 
-        <h1>Footer</h1>
+        <nav>
+            <ul className="footer">
+                <li className="footer--name">find me in: </li>
+                <section className="footer--links">
+                    <li className="footer--link" id="linkedin"><i class="fa fa-linkedin-square"></i></li>
+                    <li className="footer--link" id="insta"><i class="fa fa-instagram"></i></li>
+                </section>
+                <li className="footer--git"><i class="fa fa-github"></i>abhi-ragh</li>
+            </ul>
+        </nav>
     )
 }
